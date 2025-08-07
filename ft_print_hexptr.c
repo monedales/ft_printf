c1fb7ef95d62dd6e8d32c4dbf99a5da8446f7670
monedales <le.moneda@outlook.com>
2025-08-07 11:47:54 -0300

    ✨ function to print a pointer

--- ft_print_hexptr.c
+++ ft_print_hexptr.c
@@ -27,10 +27,22 @@ int	ft_print_hex(unsigned long n, char specifier)
 	digit = n - (quo * 16);
 	if (digit < 10)
 		chr = '0' + digit;
-	else if (digit >= 10 && specifier == 'x')
+	else if (digit >= 10 && specifier == 'x' || specifier == 'p')
 		chr = 'a' + (digit - 10);
 	else
 		chr = 'A' + (digit - 10);
 	len = len + write(1, &chr, 1);
+	return (len);
+}
+
+int	ft_printptr(unsigned long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n == 0)
+		len = ft_putstr("(nil)");
+	else
+		len = ft_putstr("0x") + ft_print_hex(n, 'p');
 	return (len);
 }
